0e0af0c38232520913420b943a8c54e7ebbe121b
Camilla Soares Sousa <camillasoares818@gmail.com>
2025-07-08 14:04:32 -0300

    feat: entities

---
+++ src/js/entities/items.js
@@ -0,0 +1,7 @@
+export const alimentos = [
+  { x: 500, y: 470 - 30, width: 20, height: 20, color: "lime", coletado: false },
+  { x: 800, y: 470 - 30, width: 20, height: 20, color: "lime", coletado: false },
+  { x: 1300, y: 470 - 30, width: 20, height: 20, color: "lime", coletado: false },
+  { x: 1900, y: 470 - 30, width: 20, height: 20, color: "lime", coletado: false },
+  { x: 2400, y: 470 - 30, width: 20, height: 20, color: "lime", coletado: false },
+];
---
+++ src/js/entities/player.js
@@ -0,0 +1,13 @@
+export const player = {
+  x: 100,
+  y: 470 - 90,
+  width: 50,
+  height: 90,
+  speed: 3,
+  color: "blue",
+  isAttacking: false,
+  attackCooldown: false,
+  isFlashing: false,
+  lives: 3,
+  alimentosColetados: 0,
+};
